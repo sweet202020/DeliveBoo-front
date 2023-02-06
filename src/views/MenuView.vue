77e5322364e3795a0bd0cf0cb300630f1437efc7
Francesca-Barcella <pollon.fra@gmail>
2023-02-06 17:35:33 +0100

    jumbotronMenu + box restaurant & order

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,42 +1,77 @@
 <script>
 import navBar from '../components/navBar.vue';
+import jumbotronMenu from '../components/jumbotronMenu.vue';
 
 export default {
   name: 'MenuView',
-  components: { navBar }
+  components: {
+    navBar,
+    jumbotronMenu
+  }
 }
 </script>
 
 <template>
   <navBar />
-  <div class="container py-5">
-    <h1>pagina singolo ristorante con menu</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, necessitatibus dolores, voluptatem velit
-      distinctio natus ratione dicta id similique, nesciunt reprehenderit voluptatum mollitia est facere magni. Fugit
-      eligendi earum illo ullam laborum rem unde cupiditate impedit eos! Tempora beatae iusto laborum magni ut eius
-      adipisci hic officia dignissimos quis porro, repudiandae vitae? Nemo, blanditiis sequi tempora, necessitatibus
-      veritatis inventore fuga dolorum magnam ad in iure eius quod ea asperiores error aliquam ipsam. Accusamus, omnis
-      assumenda, odio maiores voluptatum unde excepturi perspiciatis vero non dicta vel pariatur suscipit! Eos dolores
-      accusamus id deleniti possimus, sunt ab animi repellat distinctio quia eligendi impedit? Inventore, quia atque.
-      Cum culpa, veniam asperiores iure tempora alias ratione odio perferendis, eaque inventore atque iste aliquid
-      molestiae vero illo repudiandae! Eum quidem tempore unde dolor explicabo voluptate est. Repudiandae cum sequi
-      repellendus atque, voluptatum nostrum doloribus illum, eaque asperiores repellat, magni facilis consequatur vel
-      porro? Commodi, temporibus iure quia harum ad blanditiis dignissimos recusandae possimus dolor praesentium?
-      Incidunt quia placeat accusantium beatae eius hic ut voluptatem, doloribus, odit amet quisquam eaque sed veritatis
-      nobis saepe quam rerum corrupti aspernatur laborum nihil ex sunt ab. Nesciunt nam minima adipisci, molestias
-      cumque ipsum esse aperiam in accusantium eius impedit unde cupiditate nobis animi vero sequi consectetur est,
-      odit, atque delectus quam libero rerum laboriosam quaerat! Delectus officia aspernatur recusandae ratione autem
-      quod at dignissimos obcaecati laborum? Voluptatum rerum repudiandae quibusdam veniam! Distinctio, reiciendis
-      officia totam, quo quos dolorem quibusdam explicabo eius, quisquam eveniet earum dolores temporibus enim magnam
-      error et maiores quia fugiat similique fugit natus. Quibusdam qui repellat similique consequatur adipisci quam
-      harum quas voluptate tempora placeat at unde ea aliquam ullam in, voluptatibus culpa facere saepe iste sed
-      molestiae quisquam ipsam. Quae alias dignissimos voluptas, consequatur quod a similique explicabo. Beatae itaque
-      perspiciatis voluptate magni, maiores sunt!</p>
+  <jumbotronMenu />
+
+  <!-- restaurant & order details -->
+  <div class="container py-5 position-realtive">
+    <div class="row row-cols-1 row-cols-md-3 justify-content-center g-5">
+      <!-- restaurant details -->
+      <div class="col restaurant_details p-3 me-2">
+        <h3>Boa pizza</h3>
+        <div>pizza | bibite</div>
+        <div>Via delle vie, Il mio paese (PAESE)</div>
+        <div>Telefono: 3474067309</div>
+        <div>orari di apertura:</div>
+        <ul>
+          <li>mar-gio --> 12:00/14:00 + 19:00/22:00</li>
+          <li>ven-sab --> 12:00/14:00 + 19:00/01:00</li>
+          <li>dom --> 12:00/14:00 + 19:00/01:00</li>
+        </ul>
+      </div>
+      <!-- order details -->
+      <div class="col order_details p-3 ms-2">
+        <h3>Il tuo ordine</h3>
+        <div>pizza | bibite</div>
+        <div>Via delle vie, Il mio paese (PAESE)</div>
+        <div>Telefono: 3474067309</div>
+        <div>orari di apertura:</div>
+        <ul>
+          <li>mar-gio --> 12:00/14:00 + 19:00/22:00</li>
+          <li>ven-sab --> 12:00/14:00 + 19:00/01:00</li>
+          <li>dom --> 12:00/14:00 + 19:00/01:00</li>
+        </ul>
+      </div>
+    </div>
   </div>
+
+  <!-- menu -->
 
 </template>
 
 
 <style lang="scss" scoped>
+@use '../styles/partials/variables.scss' as *;
 
+.restaurant_details,
+.order_details {
+  background-color: #fcfcfc;
+  border: 3px solid $deliveboo-primary-light;
+  border-radius: 10px;
+  position: relative;
+  top: -100px;
+  box-shadow: 3px 3px 10px $deliveboo-primary;
+
+  h3 {
+    color: $deliveboo-primary;
+  }
+
+  div,
+  li {
+    color: $deliveboo-dark;
+    font-weight: 400;
+  }
+}
 </style>
